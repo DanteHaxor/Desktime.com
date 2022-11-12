9af3e34cc7560834292610abd69378e261ac2e9c
Abhay Singh <abhay.jhonsmith@gmail.com>
2022-11-12 17:22:00 +0530

    Added Authentication

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,20 @@
 document.getElementById("logo").addEventListener("click",()=>{
     window.location.href="../index.html"
   })
+  var data=JSON.parse(localStorage.getItem("signupdata"))
+  document.getElementById("loginbtn").addEventListener("click",auth)
+  function auth(){
+    var email=document.getElementById("email").value
+    var password=document.getElementById("password").value
+    data.forEach((ele)=>{
+      if(ele.email==email && ele.password==password){
+       return alert("Login Successful!")
+      }else{
+       return alert("Wrong Credentials")
+      }
+    })
+    setTimeout(() => {
+      return window.location.href="../index.html"
+    }, 1000)
+  }
+ 
